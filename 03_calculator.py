0acb274e62a1e3e3e712522cb563b2df4d0909df
Sara <bhaji0620@gmail.com>
2026-04-07 20:16:03 +0000

    commit

---
+++ 03_calculator.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+result1 = num1 + num2
+result2 = num1 * num2
+result3 = num1 - num2 
+result4 = num1 / num2
+print(f"The sum of {num1} and {num2} is: {result1}, the product of {num1} and {num2} is {result2}, the difference of {num1} and {num2} is {result3}, the quotient is {result4}")
