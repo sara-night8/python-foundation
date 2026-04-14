3ba5a0b165d2fc876b881cb335b75488b0b58b29
Sara <bhaji0620@gmail.com>
2026-04-14 20:21:11 +0000

    added less 3

---
+++ 04_descison.py
@@ -0,0 +1,11 @@
+sec_pass = "milkyways"
+print("Welcome to the secret candy bar vault")
+guess = input("Enter Password:")
+if guess == sec_pass :
+    print("WELCOME TO THE SECRET VAULT FULL OF MILKYWAYS")
+    print("ENJOY A MILKYWAY")
+else:
+    print("WRONG. NO CANDY FOR YOU")
+    print("Security has been alerted. BETTER RUN")
+
+    
