b450a4621b45ad7d8dae4c366d1f29b070fd00e7
Sara <bhaji0620@gmail.com>
2026-04-23 19:49:34 +0000

    vvvvvvvvvvvv

---
+++ 08_welcome.py
@@ -0,0 +1,7 @@
+def greet_user(user_name):
+    print("----------------------------------")
+    print(f"Welcome back, {user_name}")
+    print("Your Distingushed Dashboard is ready!")
+    print("----------------------------------")
+greet_user("Sara")
+greet_user("Poop")
