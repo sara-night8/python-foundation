c76b03f83f7f7c0d79f1cd1f13beedcbc503bfef
Sara <bhaji0620@gmail.com>
2026-05-05 19:44:11 +0000

    commit

--- 03_calculator.py
+++ 03_calculator.py
@@ -4,4 +4,4 @@ result1 = num1 + num2
 result2 = num1 * num2
 result3 = num1 - num2 
 result4 = num1 / num2
-print(f"The sum of {num1} and {num2} is: {result1}, the product of {num1} and {num2} is {result2}, the difference of {num1} and {num2} is {result3}, the quotient is {result4}")
+print(f"The sum of {num1} and {num2} is: {result1}, the product of {num1} and {num2} is {result2}, the difference of {num1} and {num2} is {result3}, the quotient is {result4}")33333
